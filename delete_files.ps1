e99abba0bf6655a287f06f6913f8d70a1a948ecb
MtoseD <mike.daetwyler@bluewin.ch>
2022-06-10 01:50:06 +0200

    uploaded the scripts

---
+++ delete_files.ps1
@@ -0,0 +1,11 @@
+﻿#define variables | get every file-name
+$files = Get-Content "C:\Users\Mike\Downloads\songs.txt" #fill in your own path (C:\delete.txt)
+
+
+#delete every file
+foreach($file in $files){
+
+    #delete files
+    Remove-Item "C:\temp\$file" #fill in your own path (C:\temp\)
+
+}
---
+++ writeDownFiles.ps1
@@ -0,0 +1,19 @@
+﻿#define variables
+$files = ls "C:\temp\" |WHERE -Property Attributes -NE "Directory" #fill in your own path
+$directories = "ls C:\temp\" |WHERE -Property Attributes -EQ "Directory" #fill in your own path
+
+
+#check every file
+foreach($file in $files){
+    
+    #check every directory
+    foreach($directory in $directories){
+    
+        if(Test-Path -Path "C:\temp\$directory\$file") #fill in your own path (C:\temp\...)
+        {
+            Add-Content "C:\delete.txt" "$file" #write down files
+        }  
+          
+    }
+
+}
