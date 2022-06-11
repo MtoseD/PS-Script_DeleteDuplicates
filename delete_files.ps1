8d13b3f4dbc87e013fda178b52c616e2967f9e6a
MtoseD <mike.daetwyler@bluewin.ch>
2022-06-11 22:57:36 +0200

    Update delete_files.ps1

--- delete_files.ps1
+++ delete_files.ps1
@@ -1,11 +1,8 @@
-﻿#define variables | get every file-name
+#define variables | get every file-name
 $files = Get-Content "C:\Users\Mike\Downloads\songs.txt" #fill in your own path (C:\delete.txt)
-
 
 #delete every file
 foreach($file in $files){
-
     #delete files
     Remove-Item "C:\temp\$file" #fill in your own path (C:\temp\)
-
 }
